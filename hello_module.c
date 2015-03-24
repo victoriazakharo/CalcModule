f22b546ea8b80ee434eb543192eab7d5815256d1
victoriazakharo <g8ss3uio>
2015-03-24 19:49:29 +0300

    Writing

--- hello_module.c
+++ hello_module.c
@@ -1,33 +1,74 @@
 #include <linux/module.h>
+#include <linux/kernel.h>
 #include <linux/proc_fs.h>
-#include <linux/seq_file.h>
+#include <linux/sched.h>
+#include <asm/uaccess.h>
+#include <linux/slab.h>
 
-static int hello_proc_show(struct seq_file *m, void *v) {
-  seq_printf(m, "Hello proc!\n");
-  return 0;
+int len, temp, a, b;
+
+char *msg, operand;
+
+int read_proc(struct file *filp, char *buf, size_t count, loff_t *offp)
+{
+	if (count>temp)	{
+	  count = temp;
+	}
+	temp = temp - count;
+	sprintf(msg, "%d\n", a);
+	copy_to_user(buf, msg, count);
+	if (count == 0) {
+	  temp = len;
+        }
+	return count;
 }
 
-static int hello_proc_open(struct inode *inode, struct  file *file) {
-  return single_open(file, hello_proc_show, NULL);
+
+int write_a(struct file *filp, const char *buf, size_t count, loff_t *offp)
+{
+	copy_from_user(msg, buf, count);
+	len = count;
+	temp = len;
+        long tmp;
+	kstrtol(msg, 10, &tmp);
+	a = tmp + 5;
+	return count;
 }
 
-static const struct file_operations hello_proc_fops = {
-  .owner = THIS_MODULE,
-  .open = hello_proc_open,
-  .read = seq_read,
-  .llseek = seq_lseek,
-  .release = single_release,
+
+struct file_operations result_fops = {
+  read: read_proc
 };
 
-static int __init hello_proc_init(void) {
-  proc_create("hello_proc", 0, NULL, &hello_proc_fops);
-  return 0;
+struct file_operations a_fops = {
+  write: write_a
+};
+
+/*struct file_operations b_fops = {
+  read: read_proc
+};
+
+struct file_operations operand_fops = {
+  read: read_proc
+};*/
+
+void create_new_proc_entry(void)
+{
+	proc_create("result", 0666, NULL, &result_fops);
+	proc_create("a", 0666, NULL, &a_fops);
+	msg = kmalloc(GFP_KERNEL, 100 * sizeof(char));
 }
 
-static void __exit hello_proc_exit(void) {
-  remove_proc_entry("hello_proc", NULL);
+
+int proc_init(void) {
+	create_new_proc_entry();
+	return 0;
+}
+
+void proc_cleanup(void) {
+	remove_proc_entry("hello", NULL);
 }
 
 MODULE_LICENSE("GPL");
-module_init(hello_proc_init);
-module_exit(hello_proc_exit);
+module_init(proc_init);
+module_exit(proc_cleanup);
